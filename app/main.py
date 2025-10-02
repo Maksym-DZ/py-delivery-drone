2583d66046c151194d11870f1bca60bb38daee25
Maksym Dazidov <maksymdazidov@gmail.com>
2025-10-02 14:50:12 +0200

    drone 12

--- app/main.py
+++ app/main.py
@@ -59,6 +59,8 @@ class DeliveryDrone(FlyingRobot):
             max_load_weight: float = 5,
             current_load: Cargo | None = None
     ) -> None:
+        if coords is None:
+            self.coords = [0, 0, 0]
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
